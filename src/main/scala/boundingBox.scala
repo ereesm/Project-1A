376b4109a65846ebf2aa02075ce7c091d0bb78dd
Emile Reese <emilereeseii@gmail.com>
2023-09-16 22:47:21 +0000

    Implement BoundingBox, subsequent tests

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -6,6 +6,29 @@ import Shape.*
 
 object boundingBox:
   def apply(s: Shape): Location = s match
-    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+    case Rectangle(width, height) => Location(0, 0, Rectangle(width, height)) // not yet implemented
+
+    case Location(x,y,shape) => 
+      val insideBoundingBox = apply(shape)
+      Location(x,y,insideBoundingBox.shape)
+    
+    case Ellipse(semiMajor, semiMinor) => 
+      val width = semiMajor*2
+      val height = semiMinor*2
+      Location(-semiMajor,-semiMinor, Rectangle(width,height))
+
+    case Group(shapes @ _*) =>
+      val boundingBoxes = shapes.map(apply)
+      val minX = boundingBoxes.map(_.x).min
+      val minY = boundingBoxes.map(_.y).min
+      val maxX = boundingBoxes.map(bb => bb.x + bb.shape.asInstanceOf[Rectangle].width).max
+      val maxY = boundingBoxes.map(bb => bb.y + bb.shape.asInstanceOf[Rectangle].height).max
+      Location(minX, minY, Rectangle(maxX - minX, maxY - minY))
+
+      
+      
+
+
+
 
 end boundingBox
